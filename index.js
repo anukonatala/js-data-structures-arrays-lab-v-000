11335ac07f64bda8da2f26e87c2f41d538912842
Anusha Konatala <anu.konatala@gmail.com>
2018-09-02 02:36:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+
+
+const drivers =  ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+
+function appendDriver(name){
+  newDrivers = [];
+  newDrivers = drivers;
+  newDrivers.push(name);
+}
+
+function prependDriver(name){
+  drivers.unshift(name);
+}
+
+function removeLastDriver(){
+  drivers.pop();
+}
+
+function removeFirstDriver(){
+  drivers.shift();
+}
+
+// describe('index.js', function () {
+//   describe('drivers', function () {
+//     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
+//       expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+//     });
+
+//     it('is declared with const', function () {
+//       expect(function () { drivers = []; }).to.throw(TypeError);
+//     });
+//   });
+
+  
